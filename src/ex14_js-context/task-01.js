d3ba838d5773a53d2c89b90eaaca9905cbfe2e8a
TrueIgorAB <TrueIgorAB@gmail.com>
2021-12-20 17:07:40 +0300

    "ex14_task-1_corrected"

--- src/ex14_js-context/task-01.js
+++ src/ex14_js-context/task-01.js
@@ -1,57 +1,54 @@
-const Calculator = {
-    result: 0,
-  
-    add(value) {
-      this.result += typeof value === 'number' ? value : 0;
-  
-      return this;
-    },
-  
-    substract(value) {
-      this.result -= typeof value === 'number' ? value : 0;
-  
-      return this;
-    },
-  
-    divide(value) {
-      this.result /= typeof value === 'number' ? value : 1;
-  
-      return this;
-    },
-  
-    multiply(value) {
-      this.result *= typeof value === 'number' ? value : 1;
-  
-      return this;
-    },
-  
-    getResult() {
-      setTimeout(() => this.result, 7000);
-    },
-  
-    reset() {
-      this.result = 0;
-  
-      return this;
-    },
-  
-    setState(value) {
-      this.result = value;
-  
-      return this.result;
-    },
-  
-    fetchData(callback) {
-      const calc = this;
-  
-      setTimeout(() => {
-        calc.result = this;
-        callback(calc.result);
-      }, 1000);
-    },
-  };
-  
-  const calculator = Calculator.add(100);
-  
-  calculator.setState(1);
-  
+function Calculator() {
+  this.result = 0;
+}
+
+Calculator.prototype.add = function add(value) {
+  this.result += typeof value === 'number' ? value : 0;
+
+  return this;
+};
+
+Calculator.prototype.substract = function substract(value) {
+  this.result -= typeof value === 'number' ? value : 0;
+
+  return this;
+};
+
+Calculator.prototype.divide = function divide(value) {
+  this.result /= typeof value === 'number' ? value : 1;
+
+  return this;
+};
+
+Calculator.prototype.multiply = function multiply(value) {
+  this.result *= typeof value === 'number' ? value : 1;
+
+  return this;
+};
+
+Calculator.prototype.getResult = function getResult() {
+  setTimeout(() => this.result, 7000);
+};
+
+Calculator.prototype.reset = function reset() {
+  this.result = 0;
+
+  return this;
+};
+
+Calculator.prototype.setState = function setState(value) {
+  this.result = value;
+
+  return this.result;
+};
+
+Calculator.prototype.fetchData = function fetchData(callback) {
+  const calc = this;
+
+  setTimeout(() => {
+    calc.result = this;
+    callback(calc.result);
+  }, 1000);
+};
+
+module.exports = Calculator;
